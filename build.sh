6ab9ef87767d4c7233792042d0e28e055a2604aa
leslie2046 <253605712@qq.com>
2026-02-15 20:09:32 +0800

    fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-docker build \
---build-arg HTTP_PROXY=http://192.168.0.188:21026 \
---build-arg HTTPS_PROXY=http://192.168.0.188:21026 \
--t leslie2046/langfuse:v3.151.0 -f web/Dockerfile .
-
+VERSION=v3.153.4
+./build_web.sh $VERSION
+./build_worker.sh $VERSION
