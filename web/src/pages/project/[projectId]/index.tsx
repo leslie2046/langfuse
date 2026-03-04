c33de067571fa12ec427dff7dea8302ced940a82
leslie2046 <253605712@qq.com>
2026-03-04 09:13:55 +0800

    merge

--- web/src/pages/project/[projectId]/index.tsx
+++ web/src/pages/project/[projectId]/index.tsx
@@ -34,7 +34,7 @@ import { MultiSelect } from "@/src/features/filters/components/multi-select";
 import {
   convertSelectedEnvironmentsToFilter,
   useEnvironmentFilter,
-} from "@/src/hooks/use-environment-filter";
+} from "@/src/hooks/useEnvironmentFilter";
 import { useTranslation } from "@/src/features/i18n";
 import { useV4Beta } from "@/src/features/events/hooks/useV4Beta";
 import { type ViewVersion } from "@/src/features/query";
